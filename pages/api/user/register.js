6cced8e22f5c4402cf4da6a87cb10655e5aab6fc
Rax Rai <razzz.rai@gmail.com>
2023-03-25 00:35:18 +0530

    manual user uuid generation

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -1,14 +1,21 @@
 import { PrismaClient } from '@prisma/client'
 const jwt = require('jsonwebtoken');
 const prisma = new PrismaClient()
+import { v4 as uuidv4 } from 'uuid';
+
+
 
 export default async function handler(req, res) {
     const {name, email, password} = req.body
 
   try{
-    const token = jwt.sign({ name, email, password }, process.env.JWT_SECRET);
+
+    const userId = uuidv4()
+
+    const token = jwt.sign({ name, email, password, userId }, process.env.JWT_SECRET);
     const result = await prisma.user.create({
       data: {
+        id: userId,
         name,
         email,
         accessToken : token
